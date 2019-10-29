2b38260af23df839c0a888484ceddb4cbd515f84
mac <mac@macdeiMac.local>
2019-10-29 15:16:00 +0800

    first commit

---
+++ tolxx/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tolxx
+//
+//  Created by mac on 2019/10/29.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
